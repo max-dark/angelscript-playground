0433ae0202e2ebc96a9882ca2db29f074cec8437
Max Dark <maxim.dark@gmail.com>
2024-04-18 21:29:19 +0300

    add RefGC class

--- script.cpp
+++ script.cpp
@@ -89,6 +89,115 @@ private:
     ref_counter_t refCount = 1;
 };
 
+// garbage collector support
+struct RefGC
+{
+    using counter_t = int;
+    using ref_counter_t = std::atomic<counter_t>;
+    using gc_flag_t = std::atomic_bool;
+
+    static inline constexpr auto Type = "ref_gc";
+
+    // register type
+    static void Register(asIScriptEngine *engine)
+    {
+        RefGC::engine_ptr = engine;
+
+        int r;
+        // new gc type
+        r = engine->RegisterObjectType(Type, 0, asOBJ_REF | asOBJ_GC);
+        check(r, Type);
+
+        // same as Ref
+        r = engine->RegisterObjectBehaviour(Type, asBEHAVE_FACTORY, "ref_gc@ f()", asFUNCTION(RefGC::create), asCALL_CDECL);
+        check(r, "factory");
+        r = engine->RegisterObjectBehaviour(Type, asBEHAVE_ADDREF, "void f()", asMETHOD(RefGC, AddRef), asCALL_THISCALL);
+        check(r, "add ref");
+        r = engine->RegisterObjectBehaviour(Type, asBEHAVE_RELEASE, "void f()", asMETHOD(RefGC, Release), asCALL_THISCALL);
+        check(r, "release");
+
+        // gc support
+
+        r = engine->RegisterObjectBehaviour(Type, asBEHAVE_GETREFCOUNT, "int f()", asMETHOD(RefGC, GetRefCount), asCALL_THISCALL);
+        check(r, "get ref");
+        r = engine->RegisterObjectBehaviour(Type, asBEHAVE_SETGCFLAG, "void f()", asMETHOD(RefGC, SetGCFlag), asCALL_THISCALL);
+        check(r, "set flag");
+        r = engine->RegisterObjectBehaviour(Type, asBEHAVE_GETGCFLAG, "bool f()", asMETHOD(RefGC, GetGCFlag), asCALL_THISCALL);
+        check(r, "get flag");
+        r = engine->RegisterObjectBehaviour(Type, asBEHAVE_ENUMREFS, "void f(int&in)", asMETHOD(RefGC, EnumReferences), asCALL_THISCALL);
+        check(r, "enum ref");
+        r = engine->RegisterObjectBehaviour(Type, asBEHAVE_RELEASEREFS, "void f(int&in)", asMETHOD(RefGC, RemoveAllReferences), asCALL_THISCALL);
+        check(r, "release all");
+
+        // cache type info
+        typeInfo = engine->GetTypeInfoByName(Type);
+    }
+
+    // fabric method
+    static RefGC *create()
+    {
+        not_null(engine_ptr, "engine_ptr == nullptr");
+        not_null(typeInfo, "typeInfo == nullptr");
+
+        auto obj = new RefGC{};
+        engine_ptr->NotifyGarbageCollectorOfNewObject(obj, typeInfo);
+        return obj;
+    }
+
+    void AddRef()
+    {
+        gcFlag = false;
+        ++refCount;
+    }
+
+    void Release()
+    {
+        gcFlag = false;
+        if (--refCount == 0)
+        {
+            delete this;
+        }
+    }
+
+    // mark as garbage
+    void SetGCFlag()
+    {
+        gcFlag = true;
+    }
+
+    bool GetGCFlag()
+    {
+        return gcFlag;
+    }
+
+    counter_t GetRefCount()
+    {
+        return refCount;
+    }
+
+    void EnumReferences(asIScriptEngine* engine)
+    {
+        // see dictionary add-on
+        // for each sub-object
+        //    engine->GCEnumCallback(object)
+    }
+
+    void RemoveAllReferences(asIScriptEngine* engine)
+    {
+        // release all sub-objects
+    }
+
+private:
+    static asIScriptEngine *engine_ptr;
+    static asITypeInfo *typeInfo;
+private:
+    ref_counter_t refCount = 1;
+    gc_flag_t gcFlag = false;
+};
+
+asIScriptEngine* RefGC::engine_ptr = nullptr;
+asITypeInfo* RefGC::typeInfo = nullptr;
+
 namespace utility
 {
     void print(std::string& msg)
@@ -124,6 +233,7 @@ int main(int argc, char **argv) {
     {
         int r;
         RegisterUtility(engine);
+        RefGC::Register(engine);
         // program should create module
         auto program = engine->GetModule("program", asGM_ALWAYS_CREATE);
         not_null(program, "unable create module");
